727c138996210e9aa935ef9690e95d3c922ceece
PedroSodreUFES <pedro.malini@edu.ufes.br>
2026-06-05 19:04:30 -0300

    chore: pasta de imagens

--- modelo_diabetes.py
+++ modelo_diabetes.py
@@ -6,7 +6,7 @@ df_diabetes = pd.read_csv("./dataset/diabetes.csv")
 
 sns.lineplot(
   x=range(1,11),
-  y=[x * 10 for x in range(1,11)],
+  y=[x**2 for x in range(1,11)],
   color="purple",
   linestyle='--'
 )
